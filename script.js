375a2a9daae75e782db98e347c18503abebd01bf
RudranshRakshan <89257724+RudranshRakshan@users.noreply.github.com>
2021-12-31 19:44:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var  Myname="Rudransh";
+console.log(Myname.length);
+Myname+=" "
+Myname+="Rakshan";
+console.log(Myname);
+console.log(Myname.toUpperCase());
+console.log(Myname.toLowerCase());
+
+var numm=400;
+console.log(Math.sqrt(numm));
+console.log(Math.min(4,7,1,9,101,34,67));
+console.log(Math.max(4,7,1,9,101,34,67));
+console.log(Math.pow(8,2));
